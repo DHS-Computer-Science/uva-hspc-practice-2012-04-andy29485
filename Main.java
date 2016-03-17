dd5b834cc1007991bded8cec0808baf6312fce22
Noah <AndriyZasypkin@gmail.com>
2016-03-17 16:40:08 -0400

    Wrote SOlution

--- Main.java
+++ Main.java
@@ -1,10 +1,28 @@
-//TODO: <name>
-//TODO: <date>
+//Andriy Zasypkin
+//2016-03-17
 //Practice 2012 - 04: Math Tutoring
 
-public class  {
-  public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    
+    int nCases = input.nextInt();
+    
+    for(int i=0; i<nCases; i++) {
+      System.out.printf("Case %d: ", i+1);
+      int nOrder = input.nextInt();
+      
+      for(int j=0; j<nOrder+1; j++) {
+        System.out.print((nOrder-j)*input.nextInt());
+        if(j < nOrder)
+          System.out.print(" ");
+      }
+      
+      System.out.println();
+    }
+    
     System.exit(0);
   }
 }
